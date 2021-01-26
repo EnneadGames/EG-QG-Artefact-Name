66ff3b9e46bef38684e20acefece1a56e433a499
MobiusTempus <MobiusTempus@users.noreply.github.com>
2021-01-26 09:18:56 +0000

    tweaks to macro

--- Macro-EG_QG_Artefact_Name.js
+++ Macro-EG_QG_Artefact_Name.js
@@ -5,8 +5,11 @@ let table2 = game.tables.getName("EG QG Artefact Name P2")
 let roll1 = table1.roll().results[0].text;
 let roll2 = table2.roll().results[0].text;
 
-let content = `Artefact Name: ${roll1}${roll2}.`
+let content = `<h3>Artefact Name:</h3> ${roll1}${roll2}.`
 
-ChatMessage.create({content: content})
+ChatMessage.create({
+    content: content,
+    whisper: ChatMessage.getWhisperRecipients('gm')
+    })
 
 }
